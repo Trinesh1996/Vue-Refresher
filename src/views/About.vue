33e6269ed4320257412b7cbac655b1b0e76a3222
trinesh1996 <trineshchetty@gmail.com>
2019-01-27 17:48:50 +0200

    vue refresher

---
+++ src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <span>This is a vue practice application a refresher on key concepts</span>
+  </div>
+</template>
+
+
+<style scoped>
+* {
+  text-align: center;
+  margin: 0;
+  margin-top: 2em;
+}
+
+
+</style>
